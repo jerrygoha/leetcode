1299be587c76d5e4a8d2e382e2b696637ed12476
hooooonje (Jehun Ha) <48903443+jerrygoha@users.noreply.github.com>
2022-09-23 19:44:30 +0900

    Time: 21 ms (7.81%), Space: 40.2 MB (83.83%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int[] r = new int[31];
-        r[0] = 0;
-        r[1] = 1;
-        
+
         if (n == 0 || n == 1){
             return n;
         }
         
-        r[n] = fib(n-2) + fib(n-1);
-        
-        return r[n];
+        return fib(n-2) + fib(n-1);
         
     }
 }
