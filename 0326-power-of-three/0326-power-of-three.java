e5778460c60353a5163535393a2f4ec6b643ba45
hooooonje (Jehun Ha) <48903443+jerrygoha@users.noreply.github.com>
2022-11-09 00:45:57 +0900

    Time: 25 ms (74.11%), Space: 47 MB (71.09%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,16 +1,9 @@
 class Solution {
+    static int maxPow3 = (int) Math.pow(3, 19);
+
     public boolean isPowerOfThree(int n) {
-        
-        //탈출 조건
-        if(n==1){
-            return true;
-        }
-        
-        if(n==0 || n%3!= 0){
-            return false;
-        }
-        
-        return isPowerOfThree(n/3);
-        
+        // 3^19 =1162261467, 
+        // 3^20 는 정수 범위를 초과한다. 
+        return ( n>0 &&  maxPow3%n==0);
     }
 }
