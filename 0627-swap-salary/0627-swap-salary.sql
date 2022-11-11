f12b397a4a5d2e779f8cbbf3764a070c8bd14ae6
hooooonje (Jehun Ha) <48903443+jerrygoha@users.noreply.github.com>
2022-11-11 13:34:17 +0900

    Time: 597 ms (37.54%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,8 +1,6 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 update Salary 
     set 
         sex = case 
         when sex = 'm' then 'f'
             else 'm' end
-    
-             
